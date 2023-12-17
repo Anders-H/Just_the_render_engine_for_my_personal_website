028e21abc5ccc0fb172666cccea0e257f2948beb
Anders Hesselbom <anders@winsoft.se>
2023-12-17 15:35:30 +0100

    Removed a few using.

--- AhesselbomGenerator/Blogg/BloggGenerator.cs
+++ AhesselbomGenerator/Blogg/BloggGenerator.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
-using System.Xml.Linq;
-using AhesselbomGenerator.LinkManagement;
 using AhesselbomGenerator.Xml;
 
 namespace AhesselbomGenerator.Blogg;
