4f24a454c94212b8549f5bc4612118e256b9120f
Anders Hesselbom <anders@winsoft.se>
2024-02-23 18:09:03 +0100

    Store tweets...

--- MrTweet/Program.cs
+++ MrTweet/Program.cs
@@ -1,9 +1,80 @@
-﻿
+﻿using System.Diagnostics;
+using Microsoft.Data.SqlClient;
 
+const string dSource = "Data Source=.";
+const string dName = "Initial Catalog=WebSiteTweetDatabase";
+const string iSecurity = "Integrated Security=True";
+const string tCert = "Trust Server Certificate=True";
+const string connectionString = $"{dSource};{dName};{iSecurity};{tCert}";
+using var cn = new SqlConnection(connectionString);
+cn.Open();
+using var cmdGetNewestTweets = new SqlCommand("SELECT TOP 3 [Text] FROM dbo.Tweet ORDER BY [Date] DESC", cn);
+var r = cmdGetNewestTweets.ExecuteReader();
 
-public class Tweet
+while (r.Read())
 {
-    public string Text { get; }
-    public string TweetLink { get; }
-    public string SecondaryLink { get; }
+    Console.WriteLine(r.GetString(0));
+    Console.WriteLine();
 }
+
+r.Close();
+
+Console.Write("Tweet: ");
+var text = (Console.ReadLine() ?? "").Trim();
+
+if (string.IsNullOrEmpty(text))
+    return;
+
+Console.Write("Date (YYYY-MM-DD, default is today): ");
+var date = (Console.ReadLine() ?? "").Trim();
+
+if (string.IsNullOrEmpty(date))
+    date = DateTime.Now.ToString("yyyy-MM-dd");
+
+var dateParts = date.Split('-');
+var dateYear = int.Parse(dateParts[0]);
+var dateMonth = int.Parse(dateParts[1]);
+var dateDay = int.Parse(dateParts[2]);
+
+Console.Write("Time (HH:MM, default is now): ");
+var time = (Console.ReadLine() ?? "").Trim();
+
+if (string.IsNullOrWhiteSpace(time))
+    time = DateTime.Now.ToString("hh:mm");
+
+Console.Write("Link to tweet: ");
+var tweetLink = (Console.ReadLine() ?? "").Trim();
+
+if (string.IsNullOrWhiteSpace(tweetLink))
+    return;
+
+var timeParts = time.Split(":");
+var timeHour = int.Parse(timeParts[0]);
+var timeMinute = int.Parse(timeParts[1]);
+
+var tweetDate = new DateTime(dateYear, dateMonth, dateDay, timeHour, timeMinute, 0);
+
+Console.WriteLine($"Tweet date/time: {tweetDate:yyyy-MM-dd hh:mm}");
+
+Console.Write("Save (Y/N)? ");
+var save = (Console.ReadLine() ?? "").ToLower();
+
+if (save != "y")
+    return;
+
+using var cmdSave = new SqlCommand(@"
+INSERT INTO dbo.Tweet (
+    [Text], [Date], TweetLink
+)
+VALUES (
+    @Text, @Date, @TweetLink
+)", cn);
+
+cmdSave.Parameters.AddWithValue("@Text", text);
+cmdSave.Parameters.AddWithValue("@Date", tweetDate);
+cmdSave.Parameters.AddWithValue("@TweetLink", tweetLink);
+cmdSave.ExecuteNonQuery();
+
+cn.Close();
+
+Console.WriteLine("Happy computer, good computer! Bye!");
