86b067f039fcad33edae7011f961a81ce830037f
Anders Hesselbom <anders@winsoft.se>
2024-06-26 18:47:56 +0200

    Removed caching.

--- AhesselbomGenerator/HeadGenerator.cs
+++ AhesselbomGenerator/HeadGenerator.cs
@@ -30,6 +30,9 @@ public class HeadGenerator
         return @$"<head>
 <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
 <meta charset=""utf-8"" />
+<meta http-equiv=""Cache-Control"" content=""no-cache, no-store, must-revalidate"" />
+<meta http-equiv=""Pragma"" content=""no-cache"" />
+<meta http-equiv=""Expires"" content=""Tue, 01 Jan 1980 1:00:00 GMT"" />
 <title>{title}</title>
 <link rel=""stylesheet"" href=""{level}{css}"">
 <script src=""{level}today.js""></script>
